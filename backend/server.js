ec214a2b96c7a12aa268b624186e32be1512d4c7
Bobby <boobeh321@gmail.com>
2025-07-02 15:59:16 -1000

    Add /api/scrape route. Add cors. Replace res.render with res.json

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,36 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const morgan = require('morgan');
 const { body, validationResult } = require('express-validator');
 const mongoose = require('mongoose');
-const connectDB = require('./config/database')
-const homeRoutes = require('../routes/home')
+const connectDB = require('./config/database');
+const scrapeRoutes = require('./routes/scrape');
 const PORT = process.env.PORT || 3000;
-require('dotenv').config({path: './config/.env'})
+require('dotenv').config({path: './config/.env'});
 connectDB()
 // Middleware 
+app.use(cors());
+// Restrict frontend domain in production
+// app.use(cors({
+//     origin: 'production_url',
+//     credentials: true, // if you need to send cookies/auth
+//   }));
 app.use(express.json());    // Parses JSON
 app.use(morgan('dev'));     // Logging
 
 // Routes
-// app.use('/', homeRoutes)
-
-// Basic route
 app.get('/', (req, res) => {
     res.send('Hello from ScrapeWorks backend!');
   });
-
+app.use('/api/scrape', scrapeRoutes);
 // Error handling routes
 app.use((req, res) => {
-    res.status(404).render('errors/404');
+    res.status(404).json({ error: 'Does not exist' });
 });
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).render('errors/500');
+    res.status(500).json({ error: 'Something went wrong' });
 });
 
 app.listen(PORT, ()=>{
